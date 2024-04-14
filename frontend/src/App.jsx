e52802efa61b4db5b1ccaf0dc16a63fa6c47b31c
Ravi <ravigargrud@gmail.com>
2024-04-14 14:47:14 +0530

    basic frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+
+// get user record from /user/{id} api on port 8000
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const[data, setData] = useState({})
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/getUser/1')
+      .then(response => {
+        // handle the response data
+        // console.log(response.data.email);
+        setData(response.data)
+      })
+      .catch(error => {
+        // handle the error
+        console.error(error);
+      });
+  }, []);
+
+
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.name}<br></br>
+      {data.email}<br></br>
+      {data.password}
     </>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    // Proxy all requests to 'http://localhost:8000'
+    '*': {
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\//, '') // Remove leading '/'
+    }
+  }
 })
