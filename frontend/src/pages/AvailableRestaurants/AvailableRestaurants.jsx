9a53b2afbe453b89151df4c018694d7ebeb2a183
Aryan Sachan <aryansachan2004@gmail.com>
2024-04-15 18:52:50 +0530

    frontend done

--- frontend/src/pages/AvailableRestaurants/AvailableRestaurants.jsx
+++ frontend/src/pages/AvailableRestaurants/AvailableRestaurants.jsx
@@ -1,10 +1,13 @@
 import Time from "../../Time";
 import classes from "./AvailableRestaurants.module.css";
-import MAP from '../../assets/images/map/map.png';
-import { useParams } from "react-router-dom";
+import MAP from "../../assets/images/map/map.png";
+// import { useParams } from "react-router-dom";
 
 import TopNavbar from "../../pages/TopNavbar/TopNavbar.jsx";
 import SideNavbar from "../SideNavbar/SideNavbar.jsx";
+import RestaurantDetails from "./RestaurantDetails/RestaurantDetails.jsx";
+
+import { useState } from "react";
 
 const restaurants = [
     { name: "a", wastage: `150kg` },
@@ -13,35 +16,65 @@ const restaurants = [
 ];
 
 const AvailableRestaurants = () => {
-    const params = useParams();
-    console.log(params);
+    const [showDetails, setShowDetails] = useState(true);
+    const [clickedIndex, setClickedIndex] = useState(null);
+
+    function showDetailsHandler() {
+        setShowDetails(!showDetails);
+    }
+    function handleClick(index) {
+        setClickedIndex(index);
+    }
+
+    // const params = useParams();
+    // console.log(params);
 
     return (
         <div className="availableRestaurants">
             <div className={classes.main}>
-                <TopNavbar showNavbar={true} userName={'Aryan'} location={'121 Negro Arroyo Lane'} />
+                <TopNavbar
+                    showNavbar={true}
+                    userName={"Aryan"}
+                    location={"121 Negro Arroyo Lane"}
+                />
                 <SideNavbar showNavbar={true} />
-                <div className={classes.cardContainer}>
-                    <h1>Available Restaurants</h1>
-                    <p className={classes.orders}>Orders awaiting request</p>
-                    <div className={classes.card}>
-                        <Time className={classes.time} />
-                        <img src={MAP} alt="map" />
-                        {restaurants.map((restaurant, index) => {
-                            return (
-                                <div key={index} className={classes.details}>
-                                    <div className={classes.left}>
-                                        <h1>Yellow Flame Tree</h1>
-                                        <p>1.2 kms (For 150)</p>
-                                    </div>  
-                                    <div className={classes.right}>
-                                        <button>View Details</button>
+                {showDetails ? (
+                    <div className={classes.cardContainer}>
+                        <h1>Available Restaurants</h1>
+                        <p className={classes.orders}>
+                            Orders awaiting request
+                        </p>
+                        <div className={classes.card}>
+                            <Time className={classes.time} />
+                            <img src={MAP} alt="map" />
+                            {restaurants.map((restaurant, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className={classes.details}
+                                    >
+                                        <div className={classes.left}>
+                                            <h1>{restaurant.name}</h1>
+                                            <p>1.2 kms (For 150)</p>
+                                        </div>
+                                        <div className={classes.right}>
+                                            <button
+                                                onClick={() => {
+                                                    showDetailsHandler();
+                                                    handleClick(index);
+                                                }}
+                                            >
+                                                View Details
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <RestaurantDetails closeDetails={showDetailsHandler}/>
+                )}
             </div>
         </div>
     );
